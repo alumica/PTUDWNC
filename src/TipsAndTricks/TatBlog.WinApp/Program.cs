ecdbe12ae156b2aed239ac20e5fba159eef42d7e
Kiet Nguyen <alumica812@gmail.com>
2023-02-27 11:23:54 +0700

    Upload files B

---
+++ src/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+
+// Tạo đối tượng DbContext để quản lý phiên làm việc
+// với CSDL và trạng thái của các đối tượng
+var context = new BlogDbContext();
+
+// Tạo đối tượng khởi tạo dữ liệu
+var seeder = new DataSeeder(context);
+
+// Gọi hàm Initialize để nhập dữ liệu mẫu
+seeder.Initialize();
+
+// Tạo đối tượng BlogRepository
+IBlogRepository blogRepo = new BlogRepository(context);
+
+
+
+Console.ReadLine();
